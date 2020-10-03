7746b4a3d2c876074846d1392befb708492b8374
ecosy <kevin9434@naver.com>
2020-10-03 20:47:37 +0900

    fix: change chromedriver path to abs path

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 #%%
 from selenium import webdriver
 import csv
+import os
 
 #%%
 url = "https://www.youtube.com/watch?v=lGCo8ILvauI&t=1785s"
@@ -11,7 +12,7 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
-driver = webdriver.Chrome('chromedriver', chrome_options=options)
+driver = webdriver.Chrome(os.getcwd() + '/chromedriver', chrome_options=options)
 #%%
 driver.get(url)
 driver.implicitly_wait(0.3)
