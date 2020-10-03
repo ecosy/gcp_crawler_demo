6044975e0dde856f96ae27c9da651ccebe2a9cf9
ecosy <kevin9434@naver.com>
2020-10-03 22:43:29 +0900

    fix: change bash sh and readme.md

--- bash.sh
+++ bash.sh
@@ -1,22 +1,18 @@
-sudo apt-get update
-sudo apt-get upgrade
-
+# python3 설치
 sudo apt-get install python3
 sudo apt-get install python3-pip
 python3 --version
 
+# 크롤러 패키지 설치
 pip3 install -r requirements.txt
 
+# tmux, htop, wget 설치
 sudo apt-get install tmux
-
 sudo apt-get install htop
-
-sudo apt-get install git
-git clone https://github.com/ecosy/gcp_crawler_demo.git
-
 sudo apt-get install wget
 
-# 크롬 드라이버 설치
-apt-get install -y gconf-service libappindicator3-1 libgbm1 libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
+# 크롬 드라이버 종속파일 설치
+sudo apt-get install -y gconf-service libappindicator3-1 libgbm1 libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
 
+# 크롬 드라이버 다운로드
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
--- bash_02.sh
+++ bash_02.sh
@@ -1,5 +1,6 @@
+# 크롬 드라이버 종속성 설치
 sudo apt --fix-broken install
 
+# 크롬드라이버 설치
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-
 sudo apt-get -fy install
